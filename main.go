f658fef17b58d260a83462b11a373ecfaf79ca1f
Yoga Budhi Prawira <yogabprawira@gmail.com>
2019-10-02 20:47:53 +0700

    init commit

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+func fibo(a int, b int) int {
+	c := a + b
+	fmt.Print(c, " ")
+	if c > 300 {
+		return 0
+	}
+	return fibo(b, c)
+}
+
 func main() {
-	SessionOne()
+	a := 5
+	b := 7
+	fmt.Print(a, " ", b, " ")
+	fibo(a, b)
 }
