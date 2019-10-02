aa833c75f840739eaf58e225c3f9a40a45d46297
Fauzan Rabbani <jhanojan@gmail.com>
2019-10-02 21:58:52 +0700

    exercise 2 oktober

---
+++ fauzan/bilanganprima-deret.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	prima(3, 10)
+	deret(1, 10, 3)
+}
+func prima(start, n int) {
+	var g, a, i int
+
+	fmt.Print("Barisan Angka Prima -> ")
+	count := 0
+	for i = start; i <= 200; i++ {
+		if count == n {
+			fmt.Print("\n")
+			break
+		}
+		a = 0
+		for g = 1; g <= i; g++ {
+			if i%g == 0 {
+				a++
+			}
+		}
+
+		if a == 2 {
+			fmt.Print(i, " ")
+			count = count + 1
+		}
+
+	}
+}
+
+func deret(start, n, mp int) {
+	var i int
+
+	fmt.Print("Deret Angka -> ")
+	count := 0
+	next := start
+	for i = start; i <= 200; i++ {
+
+		if count == n {
+			break
+		}
+
+		fmt.Print(next, " ")
+		next = next + mp
+		mp = mp + 2
+		count = count + 1
+	}
+}
