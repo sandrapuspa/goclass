30ee0b9f572831d29d1bf92b013a5b807cd7ac53
ardhiansyahputra <ardhimetanaputra@gmail.com>
2019-10-02 21:05:56 +0700

    fibonacci

--- main.go
+++ main.go
@@ -5,24 +5,6 @@ import (
 	fibonacci "goclass/fibonacci"
 )
 
-// func fibonacci(val1, val2, limit int) []int {
-// 	var result = []int{val1, val2}
-
-// 	for {
-// 		val3 := val1 + val2
-// 		val1 = val2
-// 		val2 = val3
-
-// 		if (val3 > limit) {
-// 			break
-// 		}
-
-// 		result = append(result, val3)
-// 	}
-
-// 	return result
-// }
-
 func main() {
 	f := fibonacci.Init{
 		5, 7, 500
