963afa6f2b7e27dd36d16922d92510e2a1b36aaa
andigaluh <andi@komunigrafik.com>
2019-10-02 21:57:21 +0700

    upload package andi

---
+++ andi/fibo.go
@@ -0,0 +1,27 @@
+package main
+ 
+import "fmt"
+ 
+func main(){
+    var n int
+    t1:=5
+    t2:=7
+    nextnya:=0
+    n = 10
+     
+    fmt.Print("TASK 1: Barisan Fibonacci : ")
+    for i:=1;i<=n;i++ {
+        if(i==1){
+            fmt.Print(" ",t1)
+            continue
+        }
+        if(i==2){
+            fmt.Print(" ",t2)
+            continue
+        }
+        nextnya = t1 + t2
+        t1 = t2
+        t2 = nextnya
+        fmt.Print(" ",nextnya)
+    }
+}
