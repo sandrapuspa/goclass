a3483fc9ea4f4fbbb6f090c5dd263d27b91384de
patrick liviyanto <ptrickliviyanto@gmail.com>
2019-10-02 21:10:56 +0700

    patrick fibonacci

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	SessionOne()
+	fibonacci := []int{5}
+	returnData := []int{2}
+
+	for i := 0; i < 10; i++ {
+		var return2, totalNumber = Sum(returnData[i], fibonacci[i])
+		returnData = append(returnData, return2)
+		fibonacci = append(fibonacci, totalNumber)
+	}
+	fmt.Println(fibonacci)
+}
+
+func Sum(num1 int, num2 int) (int, int) {
+	var sumNumber = num1 + num2
+	return num2, sumNumber
 }
