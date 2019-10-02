8fb3ea0c3edcaefd009d36c9e50efdfd116d704e
Arborea Ramdhan Feriyanto <arborea.arf@gmail.com>
2019-10-02 21:39:11 +0700

    testo

---
+++ Ibob/prime.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+// return list of primes less than N
+func prima(N int) (primes []int) {
+    b := make([]bool, N)
+    for i := 2; i < N; i++ {
+        if b[i] == true { continue }
+        primes = append(primes, i)
+        for k := i * i; k < N; k += i {
+            b[k] = true
+        }
+    }
+    return
+}
+
+func main() {
+    primes := prima(30)
+    for _, p := range primes {
+        fmt.Print(p,", ")
+    }
+}
